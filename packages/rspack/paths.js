a515ebad226fb13d5160064ca311ec900effc7b3
Oleksandr Mezhenskyi <oleksandr.mezhenskyi@agr-software.com>
2025-05-16 15:31:34 +0200

    remove ts cheker plugin

--- packages/rspack/paths.js
+++ packages/rspack/paths.js
@@ -5,11 +5,14 @@ const appDirectory = fs.realpathSync(process.cwd());
 const rootDirectory = path.resolve(__dirname, "../../");
 
 const resolveRoot = (relativePath) => path.resolve(rootDirectory, relativePath);
+const resolveApp = (...relativePath) =>
+  path.resolve(appDirectory, ...relativePath);
 
 module.exports = {
   appDirectory,
   rootDirectory,
   resolveRoot,
+  resolveApp,
   appsDirectory: resolveRoot("apps"),
   appBuild: resolveRoot("build"),
   appHtml: path.resolve(__dirname, "./parts/template.ejs"),
